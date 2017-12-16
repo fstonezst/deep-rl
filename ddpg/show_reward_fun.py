670efabe5f8d069a530e6b806fcc6e13b006bd93
fstone <fstone.zh@gmail.com>
2017-12-16 21:02:31 +0800

    modify show reward fun to 3D

--- ddpg/show_reward_fun.py
+++ ddpg/show_reward_fun.py
@@ -18,16 +18,31 @@ def speed_fun(speed):
     # return 0.1 / (np.square(idddpg/show_reward_fun.py:13dpg/show_reward_fun.py:13) + 0.001)
     # return 1 / (i + 0.005)ddpg/show_reward_fun.py:13
 
-step, start, end = 0.01, 0, 9 #AGV.MAX_SPEED
-scalEnd = int(end * (1 / step))
-list = [error_fun(i * step) for i in range(0, scalEnd)]
+# step, start, end = 0.01, 0, 9 #AGV.MAX_SPEED
+# scalEnd = int(end * (1 / step))
+# list = [error_fun(i * step) for i in range(0, scalEnd)]
+#
+# step, start, end = 0.01, 0, 2 #PathFollowingV2.error_bound
+# scalEnd = int(end * (1 / step))
+# list2 = [speed_fun(i * step) for i in range(0, scalEnd)]
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1,1,1)
+# ax.plot(list,'r-')
+# ax.plot(list2,'b-')
+# plt.show()
 
-step, start, end = 0.01, 0, 2 #PathFollowingV2.error_bound
-scalEnd = int(end * (1 / step))
-list2 = [speed_fun(i * step) for i in range(0, scalEnd)]
 
+from mpl_toolkits.mplot3d import Axes3D
+
+step, start, end = 0.05, 0, 2 #AGV.MAX_SPEED
+X = np.arange(start, end, step)
+step, start, end = 0.1 , 0, 4 #PathFollowingV2.error_bound
+Y = np.arange(start, end, step)
+X, Y = np.meshgrid(X, Y)
+Z = np.matrix(map(error_fun, X)) + np.matrix(map(speed_fun, Y))
+Z = np.array(Z)
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.plot(list,'r-')
-ax.plot(list2,'b-')
+ax = Axes3D(fig)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
 plt.show()
