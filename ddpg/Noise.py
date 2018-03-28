3ca1b406624fbe0500725232155d60e6b5039271
fstonezst <fstone.zh@gmail.com>
2018-03-28 20:56:41 +0800

    Add noise draw table

--- ddpg/Noise.py
+++ ddpg/Noise.py
@@ -1,10 +1,10 @@
+# -- coding: utf-8 --
 # ===========================
 #   Noise - Ornstein Uhlenbeck
 #   Modified from: http://www.turingfinance.com/random-walks-down-wall-street-stochastic-processes-in-python/
 #   Author: Liam Pettigrew
 # ===========================
 import numpy as np
-
 
 class OrnsteinUhlenbeckNoise(object):
 
@@ -38,6 +38,7 @@ class OrnsteinUhlenbeckNoise(object):
         randomness = self.brownian_motion_log_returns()
         return prev_ou_level + drift + randomness
 
+# import random
 # # Noise parameters - Ornstein Uhlenbeck
 # import matplotlib.pyplot as plt
 # DELTA = 0.1 # The rate of change (time)
@@ -46,25 +47,41 @@ class OrnsteinUhlenbeckNoise(object):
 # OU_MU = 0. # The long run average interest rate
 #
 # ou_level = 0.
-# noise = Noise(DELTA, SIGMA, OU_A, OU_MU)
+# noise = OrnsteinUhlenbeckNoise(DELTA, SIGMA, OU_A, OU_MU)
 # record = []
+# random_record = []
 #
 # for _ in range(100):
 #     ou_level = noise.ornstein_uhlenbeck_level(ou_level)
 #     record.append(ou_level)
+#     random_record.append((random.randint(-50,50)*0.1))
 #
 # fig = plt.figure()
-# ax = fig.add_subplot(1,1,1)
-# ax.plot(record,'r-o')
-# plt.show()
+# ax = fig.add_subplot(1, 1, 1)
+# ax.plot(record, 'k-', label='Ornstein Uhlenbeck noise')
+# ax.grid(True)
+# # ax.legend(loc='best')
+# ax.set_xlabel(u"时间")
+# ax.set_ylabel(u"噪音大小")
 #
-# # total, max, min = 0, -1, 1
-# # for _ in range(100000):
-# #     ou_level = noise.ornstein_uhlenbeck_level(ou_level)
-# #     if ou_level > max:
-# #         max =ou_level
-# #     if ou_level < min:
-# #         min = ou_level
-# #     total += ou_level
-# # print max,min,total,total/100.0
+# fig1 = plt.figure()
+# ax1 = fig1.add_subplot(1, 1, 1)
+# ax1.plot(random_record, 'k-', label='random noise')
+# ax1.set_xlabel(u"时间")
+# ax1.set_ylabel(u"噪音大小")
+# ax1.grid(True)
+# # ax1.legend(loc='best')
+# plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+# plt.show()
+
+# total, max, min = 0, -1, 1
+# for _ in range(100000):
+#     ou_level = noise.ornstein_uhlenbeck_level(ou_level)
+#     if ou_level > max:
+#         max =ou_level
+#     if ou_level < min:
+#         min = ou_level
+#     total += ou_level
+# print max,min,total,total/100.0
 
